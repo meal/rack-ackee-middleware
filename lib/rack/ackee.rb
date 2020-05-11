07845463f616f1f1b5be91307d228fa286c1d15b
Mateusz Kozak <mateusz@mkozak.pl>
2020-05-11 18:05:40 +0200

    try to get full url

--- lib/rack/ackee.rb
+++ lib/rack/ackee.rb
@@ -29,7 +29,7 @@ module Rack
       ua_header = env['HTTP_USER_AGENT']
       user_agent = UserAgentParser.parse(ua_header)
       data = {
-            "siteLocation": env['REQUEST_URI'],
+            "siteLocation": env.url,
             "siteReferrer": env['HTTP_REFERER'],
             "deviceName": user_agent.device.model,
             "deviceManufacturer": user_agent.device.brand,
